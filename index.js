010d9a683b76c1079a06b7a359abb29654194edd
leovcunha <lvcunha@gmail.com>
2018-07-20 20:25:45 +0000

    started frontend

---
+++ index.js
@@ -0,0 +1,13 @@
+import React from "react";
+import ReactDOM from "react-dom";
+import { HashRouter } from "react-router-dom";
+import App from "./src/app";
+
+
+ReactDOM.render(
+    (
+        <HashRouter>
+            <App/>
+        </HashRouter>
+    ), document.getElementById("root")
+);
---
+++ webpack.config.js
@@ -0,0 +1,90 @@
+const path = require("path");
+const webpack = require("webpack");
+const HtmlWebpackPlugin = require("html-webpack-plugin");
+const MiniCssExtractPlugin = require("mini-css-extract-plugin");
+const UglifyJsPlugin = require('uglifyjs-webpack-plugin');
+
+module.exports = (env, argv) => (
+    {
+    entry: "./index.js",
+
+    output: {
+        path: path.resolve(__dirname, "dist"),
+        filename: "bundle.js",
+        publicPath: "/"
+    },
+    devtool: "inline-source-map",
+    devServer: {
+        historyApiFallback: true
+    },
+
+    plugins: argv.mode === "production" ? [
+
+        new HtmlWebpackPlugin({ // production
+            hash: false,
+            template: "./index.html",
+            filename: path.join(__dirname, "dist", "index.html")
+        }),
+
+        new MiniCssExtractPlugin({
+            filename:  "[name].css",
+            chunkFilename: "[id].css"
+
+        }),
+
+        new UglifyJsPlugin(
+        {
+             uglifyOptions: {
+              output: {
+                comments: false,
+                beautify: false,
+              }
+             }
+        }),
+    ] : [
+            new HtmlWebpackPlugin({ // production
+            hash: false,
+            template: "./index.html",
+            filename: path.join(__dirname, "dist", "index.html")
+        }),
+        ]
+    ,
+
+    module: {
+        rules: [
+            {
+                test: /\.js$/,
+                exclude: /(node_modules|bower_components)/,
+                use: {
+                    loader: "babel-loader",
+                    options: {
+                        presets: ["env", "react"]
+                    }
+                }
+            },
+            {
+                test: /\.s?css$/,
+                use: [
+                     argv.mode === "production" ? MiniCssExtractPlugin.loader : "style-loader",
+                    "css-loader", // translates CSS into CommonJS
+                    "sass-loader" // compiles Sass to CSS
+                ]
+            },
+            {
+                test: /\.(jpe?g|png|gif|ico)$/i,
+                use: [
+                    {
+                        loader: "file-loader",
+                        options: {
+                            name: "[name].[ext]",
+                            outputPath: "assets",
+                            publicPath: "/assets"
+                        }
+                    }
+                ]
+            },
+        ]
+    }
+
+
+});
