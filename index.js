7f1f0ae8cc9b8bbbf54077d226214cb7556809a0
leovcunha <lvcunha@gmail.com>
2018-08-17 14:43:54 +0000

    redux config

--- index.js
+++ index.js
@@ -1,13 +1,23 @@
 import React from "react";
 import ReactDOM from "react-dom";
 import { BrowserRouter } from "react-router-dom";
+import { Provider } from "react-redux";
+import { createStore, applyMiddleware } from "redux";
+import thunk from "redux-thunk"
+import rootReducer from "./reducers"
 import App from "./src/app";
 
+const store = createStore(
+  rootReducer,
+  applyMiddleware(thunk)
+);
 
 ReactDOM.render(
     (
-        <BrowserRouter>
-            <App/>
-        </BrowserRouter>
+        <Provider store={store}>
+            <BrowserRouter>
+                <App/>
+            </BrowserRouter>
+        </Provider>
     ), document.getElementById("root")
 );
