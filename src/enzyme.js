b2737f368da8dfac4a41eb02f20da180f97a27ee
leovcunha <lvcunha@gmail.com>
2018-09-30 17:51:10 -0300

    (bugged commit)

--- src/enzyme.js
+++ src/enzyme.js
@@ -1,8 +1,4 @@
-import Enzyme, {
-    configure, shallow, mount, render
-} from "enzyme";
+import Enzyme from "enzyme";
 import Adapter from "enzyme-adapter-react-16";
 
-configure({ adapter: new Adapter() });
-export { shallow, mount, render };
-export default Enzyme;
+Enzyme.configure({ adapter: new Adapter() });
