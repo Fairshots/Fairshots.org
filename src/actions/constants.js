32349784e75cae0ef431186607cb46ef43273220
leovcunha <lvcunha@gmail.com>
2018-12-12 23:00:02 -0200

    include dev server

--- src/actions/constants.js
+++ src/actions/constants.js
@@ -1,4 +1,4 @@
 const CLOUDINARY_API = "https://api.cloudinary.com/v1_1/fairshots/image";
-const FAIRSHOTS_API = "https://node-lvcunha.c9users.io:8080/";
+const FAIRSHOTS_API = "http://fairshots-dev-server.herokuapp.com/";
 
 export { CLOUDINARY_API, FAIRSHOTS_API };
