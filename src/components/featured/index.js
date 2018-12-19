d294881c9b9c39ce7cb9877352cac948b5cfb0df
leovcunha <lvcunha@gmail.com>
2018-12-19 17:32:04 -0200

    still on

--- src/components/featured/index.js
+++ src/components/featured/index.js
@@ -5,54 +5,88 @@ import {
 
 import "./featured.scss";
 
-export default function Featured(props) {
-    const { feats } = props;
-    return (
-        <div className="featured">
+export default class Featured extends Component {
+    constructor(props) {
+        super(props);
+        this.state = {
+            zoomCardDeck: {
+                transform: "scaleX(0.85) scaleY(0.85) scaleZ(1)",
+                transition: "transform 800ms ease 0s"
+            }
+        };
+    }
 
-            <h2 className="feautured-h3">Featured photographers</h2>
-            <CardDeck>
-                {
-                    feats.photographers
-                        ? feats.photographers.map(photographer => (<Card
-                            key={photographer.id}>
-                            <div className="card-img-top-holder">
-                                <CardImg top src={photographer.Photos.length > 1 ? photographer.Photos[0] : photographer.ProfilePic} alt="Card image cap" />
-                                <CardImgOverlay className="feat-biography">
-                                    <CardSubtitle >{photographer.Biography}</CardSubtitle>
-                                </CardImgOverlay>
-                            </div>
-                            <CardBody>
-                                <CardImg className="feat-pic" src={photographer.ProfilePic}/>
-                                <CardTitle>{photographer.Name}</CardTitle>
-                                <CardSubtitle>{`${photographer.Skill} Photographer` }</CardSubtitle>
-                                <CardText>{photographer.Country}</CardText>
-                            </CardBody>
-                        </Card>)) : "Loading..."
+
+    componentDidUpdate() {
+        if (window.scrollY <= 1400) {
+            this.setState({
+                zoomCardDeck: {
+                    transform: "scaleX(0.85) scaleY(0.85) scaleZ(1)",
+                    transition: "transform 800ms ease 0s"
                 }
-
-            </CardDeck>
-
-            <a href="photographers.html" className="text-link see-all">see all available photographers</a>
-
-            <h2 className="feautured-h3">Featured ORGANIZATIONS</h2>
-
-            <CardDeck>
-                {
-                    feats.organizations
-                        ? feats.organizations.map(org => (<Card key={org.id}
-                        >
-                            <CardImg top width="100%" src={org.ProfilePic} alt="Card image cap" />
-                            <CardBody>
-                                <CardImg className="feat-pic" src={org.Logo}/>
-                                <CardTitle>{org.Name}</CardTitle>
-                                <CardText>{`Based in ${org.Country}`}</CardText>
-                            </CardBody>
-                        </Card>)) : "Loading..."
+            });
+        } else if (window.scrollY >= 1400) {
+            this.setState({
+                zoomCardDeck: {
+                    transform: "scaleX(1) scaleY(1) scaleZ(1)",
+                    transition: "transform 800ms ease 0s"
                 }
+            });
+        }
+    }
 
-            </CardDeck>
-            <a href="organizations.html" className="text-link see-all">see all available ORGANIZATIONS</a>
-        </div>
-    );
+
+    render() {
+        const { feats } = this.props;
+        const { zoomCardDeck } = this.state;
+
+        return (
+            <div className="featured">
+
+                <h2 className="feautured-h3">Featured photographers</h2>
+                <CardDeck style={zoomCardDeck}>
+                    {
+                        feats.photographers
+                            ? feats.photographers.map(photographer => (<Card
+                                key={photographer.id}>
+                                <div className="card-img-top-holder">
+                                    <CardImg top src={photographer.Photos.length > 1 ? photographer.Photos[0] : photographer.ProfilePic} alt="Card image cap" />
+                                    <CardImgOverlay className="feat-biography">
+                                        <p>{photographer.Biography}</p>
+                                    </CardImgOverlay>
+                                </div>
+                                <CardBody>
+                                    <CardImg className="feat-pic" src={photographer.ProfilePic}/>
+                                    <CardTitle>{photographer.Name}</CardTitle>
+                                    <CardSubtitle>{`${photographer.Skill} Photographer` }</CardSubtitle>
+                                    <CardText>{photographer.Country}</CardText>
+                                </CardBody>
+                            </Card>)) : "Loading..."
+                    }
+
+                </CardDeck>
+
+                <a href="photographers.html" className="text-link see-all mb-3">see all available photographers</a>
+
+                <h2 className="feautured-h3">Featured ORGANIZATIONS</h2>
+
+                <CardDeck>
+                    {
+                        feats.organizations
+                            ? feats.organizations.map(org => (<Card key={org.id}
+                            >
+                                <CardImg top width="100%" src={org.ProfilePic} alt="Card image cap" />
+                                <CardBody>
+                                    <CardImg className="feat-pic" src={org.Logo}/>
+                                    <CardTitle>{org.Name}</CardTitle>
+                                    <CardText>{`Based in ${org.Country}`}</CardText>
+                                </CardBody>
+                            </Card>)) : "Loading..."
+                    }
+
+                </CardDeck>
+                <a href="organizations.html" className="text-link see-all mb-3">see all available ORGANIZATIONS</a>
+            </div>
+        );
+    }
 }
