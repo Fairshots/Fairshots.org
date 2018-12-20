f15dfe012c6e7c679ee8b4aba6b7036bf9545828
leovcunha <lvcunha@gmail.com>
2018-12-20 17:24:34 -0200

    feats done except links

--- src/components/featured/index.js
+++ src/components/featured/index.js
@@ -9,46 +9,66 @@ export default class Featured extends Component {
     constructor(props) {
         super(props);
         this.state = {
-            zoomCardDeck: {
+            zoomPhotographersDeck: {
+                transform: "scaleX(0.85) scaleY(0.85) scaleZ(1)",
+                transition: "transform 800ms ease 0s"
+            },
+            zoomOrgsDeck: {
                 transform: "scaleX(0.85) scaleY(0.85) scaleZ(1)",
                 transition: "transform 800ms ease 0s"
             }
         };
     }
 
-
-    componentDidUpdate() {
-        if (window.scrollY <= 1400) {
-            this.setState({
-                zoomCardDeck: {
-                    transform: "scaleX(0.85) scaleY(0.85) scaleZ(1)",
-                    transition: "transform 800ms ease 0s"
-                }
-            });
-        } else if (window.scrollY >= 1400) {
-            this.setState({
-                zoomCardDeck: {
-                    transform: "scaleX(1) scaleY(1) scaleZ(1)",
-                    transition: "transform 800ms ease 0s"
-                }
-            });
-        }
+    componentDidMount() {
+        window.onscroll = () => {
+            if (window.scrollY <= 1100) {
+                this.setState({
+                    zoomPhotographersDeck: {
+                        transform: "scaleX(0.85) scaleY(0.85) scaleZ(1)",
+                        transition: "transform 800ms ease 0s"
+                    }
+                });
+            } else if (window.scrollY >= 1100) {
+                this.setState({
+                    zoomPhotographersDeck: {
+                        transform: "scaleX(1) scaleY(1) scaleZ(1)",
+                        transition: "transform 800ms ease 0s"
+                    }
+                });
+            }
+            if (window.scrollY <= 1400) {
+                this.setState({
+                    zoomOrgsDeck: {
+                        transform: "scaleX(0.85) scaleY(0.85) scaleZ(1)",
+                        transition: "transform 800ms ease 0s"
+                    }
+                });
+            } else if (window.scrollY >= 1400) {
+                this.setState({
+                    zoomOrgsDeck: {
+                        transform: "scaleX(1) scaleY(1) scaleZ(1)",
+                        transition: "transform 800ms ease 0s"
+                    }
+                });
+            }
+        };
     }
 
 
     render() {
         const { feats } = this.props;
-        const { zoomCardDeck } = this.state;
+        const { zoomPhotographersDeck, zoomOrgsDeck } = this.state;
 
         return (
             <div className="featured">
 
                 <h2 className="feautured-h3">Featured photographers</h2>
-                <CardDeck style={zoomCardDeck}>
+                <CardDeck>
                     {
                         feats.photographers
                             ? feats.photographers.map(photographer => (<Card
-                                key={photographer.id}>
+                                key={photographer.id} style={zoomPhotographersDeck}>
                                 <div className="card-img-top-holder">
                                     <CardImg top src={photographer.Photos.length > 1 ? photographer.Photos[0] : photographer.ProfilePic} alt="Card image cap" />
                                     <CardImgOverlay className="feat-biography">
@@ -73,7 +93,7 @@ export default class Featured extends Component {
                 <CardDeck>
                     {
                         feats.organizations
-                            ? feats.organizations.map(org => (<Card key={org.id}
+                            ? feats.organizations.map(org => (<Card key={org.id} style={zoomOrgsDeck}
                             >
                                 <CardImg top width="100%" src={org.ProfilePic} alt="Card image cap" />
                                 <CardBody>
