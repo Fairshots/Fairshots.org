a0963b50a8f43416de6736b4a1c87ee79e596df3
leovcunha <lvcunha@gmail.com>
2019-01-04 17:12:18 -0200

    reworking user profile to be usable to any profile (not only self)

--- src/components/photogProfile/index.js
+++ src/components/photogProfile/index.js
@@ -1,20 +1,20 @@
 import React from "react";
-import {
-    Button
-} from "reactstrap";
+import { Button } from "reactstrap";
 
 import Portfolio from "../portfolio";
 
 import "./photogProfile.scss";
 
-
 export default function PhotogProfile({ photographer, uploadPhoto, toggleModal }) {
     const { cloudinary } = window;
-    const imgUploadWidget = cloudinary.createUploadWidget({ cloudName: "fairshots", uploadPreset: "kahvrgme" }, (error, result) => {
-        if (result.event === "success") {
-            uploadPhoto(result.info.secure_url);
+    const imgUploadWidget = cloudinary.createUploadWidget(
+        { cloudName: "fairshots", uploadPreset: "kahvrgme" },
+        (error, result) => {
+            if (result.event === "success") {
+                uploadPhoto(result.info.secure_url);
+            }
         }
-    });
+    );
 
     return (
         <div className="photogProfile container">
@@ -27,24 +27,45 @@ export default function PhotogProfile({ photographer, uploadPhoto, toggleModal }
                     <p className="listing-subtittle page">{`${photographer.Skill} Photographer`}</p>
                     <p className="general-paragraph">{photographer.Biography}</p>
                 </div>
-
             </div>
             <div className="row">
                 <div className="col-sm-3 d-flex flex-column align-items-center">
-                    <Button color="success w-75 mb-2" onClick={() => toggleModal("UPLOAD_PROFILE")}>Edit Profile</Button>
-                    <Button color="success w-75 mb-2" onClick={() => imgUploadWidget.open()}>Upload Photos</Button>
-                    <Button color="secondary w-75 mb-2" onClick={() => toggleModal("INACTIVATE_PROFILE")}>
-                        {photographer.accountInactive ? "Reactivate" : "Inactivate"} Profile</Button>
+                    <Button color="success w-75 mb-2" onClick={() => toggleModal("UPLOAD_PROFILE")}>
+                        Edit Profile
+                    </Button>
+                    <Button color="success w-75 mb-2" onClick={() => imgUploadWidget.open()}>
+                        Upload Photos
+                    </Button>
+                    <Button
+                        color="secondary w-75 mb-2"
+                        onClick={() => toggleModal("INACTIVATE_PROFILE")}
+                    >
+                        {photographer.accountInactive ? "Reactivate" : "Inactivate"} Profile
+                    </Button>
                 </div>
                 <div className="col-sm-9 d-flex">
                     <div className="col-sm-6 d-flex p-0">
-                        <img src="/images/place.png" height="40"/>
-                        <p className="general-paragraph ml-2">{`Based in ${photographer.City}, ${photographer.Country}`} </p>
+                        <img src="/images/place.png" height="40" />
+                        <p className="general-paragraph ml-2">
+                            {`Based in ${photographer.City}, ${photographer.Country}`}{" "}
+                        </p>
                     </div>
                     <div className="col-sm-6">
-                        {photographer.webpage && <a href={photographer.webpage} className="text-link">{photographer.webpage}</a>}
-                        {photographer.facebook && <a href={photographer.facebook} className="text-link">{photographer.facebook}</a>}
-                        {photographer.instagram && <a href={photographer.instagram} className="text-link">{photographer.instagram}</a>}
+                        {photographer.webpage && (
+                            <a href={photographer.webpage} className="text-link">
+                                {photographer.webpage}
+                            </a>
+                        )}
+                        {photographer.facebook && (
+                            <a href={photographer.facebook} className="text-link">
+                                {photographer.facebook}
+                            </a>
+                        )}
+                        {photographer.instagram && (
+                            <a href={photographer.instagram} className="text-link">
+                                {photographer.instagram}
+                            </a>
+                        )}
                     </div>
                 </div>
             </div>
@@ -52,7 +73,11 @@ export default function PhotogProfile({ photographer, uploadPhoto, toggleModal }
                 <h3 className="portfolio-tittle">Portfolio</h3>
             </div>
             <div className="portfolio-holder row justify-content-center">
-                {photographer.Photos ? <Portfolio photos={photographer.Photos} toggleDelPhoto={toggleModal}/> : <p>Loading...</p> }
+                {photographer.Photos ? (
+                    <Portfolio photos={photographer.Photos} toggleDelPhoto={toggleModal} />
+                ) : (
+                    <p>Loading...</p>
+                )}
             </div>
         </div>
     );
