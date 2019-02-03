36212842304ca7996443420b312428830e9a98fc
Anna Morawska <ania.morawska92@gmail.com>
2019-02-03 11:02:44 +0100

    Add new protected route to create-project page

--- src/components/main.js
+++ src/components/main.js
@@ -3,14 +3,15 @@ import { Switch, Route } from "react-router-dom";
 import Home from "./home";
 import About from "./about";
 import Contact from "./contact";
-
 import TermsandConditions from "./terms-and-conditions";
 import RegisterForm from "../containers/registerForm";
 import UserProfile from "../containers/userProfile";
 import AllOrgs from "../containers/allOrgs";
 import AllPhotographers from "../containers/allPhotographers";
+import CreateProject from "../containers/createProject";
+import { ProtectedRoute } from "./UI/";
 
-export default function Main() {
+export default function Main(props) {
     return (
         <main>
             <Switch>
@@ -21,6 +22,12 @@ export default function Main() {
                 <Route path="/photographers" exact component={AllPhotographers} />
                 <Route path="/organizations" exact component={AllOrgs} />
                 <Route path="/terms-and-conditions" exact component={TermsandConditions} />
+                <ProtectedRoute
+                    isAuthenticated={props.isAuthenticated}
+                    path="/create-a-project"
+                    exact
+                    component={CreateProject}
+                />
                 <Route path="/:userType/:userId" component={UserProfile} />
             </Switch>
         </main>
