ebfa7539ea74b8c6e885b26c1bc45fef699764a4
Anna Morawska <ania.morawska92@gmail.com>
2019-02-03 11:46:31 +0100

    Refactor navbar

--- src/components/UI/navbar.js
+++ src/components/UI/navbar.js
@@ -1,80 +1,99 @@
 import React, { Component } from "react";
-import { Link } from "react-router-dom";
-import LoginHandler from "../containers/loginHandler";
+import {
+    MDBNavbar,
+    MDBNavbarBrand,
+    MDBNavbarNav,
+    MDBNavItem,
+    MDBNavLink,
+    MDBNavbarToggler,
+    MDBCollapse,
+    MDBDropdown,
+    MDBDropdownToggle,
+    MDBDropdownMenu,
+    MDBDropdownItem
+} from "mdbreact";
+import LoginHandler from "../../containers/loginHandler";
 
-export default class NavBar extends Component {
+class NavbarPage extends Component {
+    state = {
+        isOpen: false
+    };
+
+    toggleCollapse = () => {
+        this.setState({ isOpen: !this.state.isOpen });
+    };
+
+    redirect = adress => () => {
+        this.props.history.push(adress);
+    };
+
     render() {
         return (
-            <div
-                data-collapse="medium"
-                data-animation="default"
-                data-duration="400"
-                className="navbar w-nav"
-            >
-                <div className="d-flex justify-content-between align-items-center w-100">
-                    <Link to="/" className="brand w-nav-brand">
-                        <img src="images/Fair-shotsV3.png" width="250" />
-                    </Link>
-                    <nav role="navigation" className="nav-menu w-nav-menu">
-                        <div data-delay="0" data-hover="1" className="navbarlink w-dropdown">
-                            <div className="navbarlink w-dropdown-toggle">
-                                <div>Organizations</div>
-                                <div className="w-icon-dropdown-toggle" />
-                            </div>
-                            <nav className="w-dropdown-list">
-                                <Link
-                                    to="/register#organization"
-                                    className="dropdown-link w-dropdown-link"
-                                >
-                                    register an organization
-                                </Link>
-                                <a
-                                    href="create-a-project.html"
-                                    className="dropdown-link w-dropdown-link"
-                                >
-                                    create a NEW PROJECT
-                                </a>
-                                <Link to="/photographers" className="dropdown-link w-dropdown-link">
-                                    find a photographer
-                                </Link>
-                            </nav>
-                        </div>
-                        <div data-delay="0" data-hover="1" className="navbarlink w-dropdown">
-                            <div className="navbarlink w-dropdown-toggle">
-                                <div>photographers</div>
-                                <div className="w-icon-dropdown-toggle" />
-                            </div>
-                            <nav className="w-dropdown-list">
-                                <Link
-                                    to="/register#photographer"
-                                    className="dropdown-link w-dropdown-link"
-                                >
-                                    register as a photographer
-                                </Link>
-                                <Link to="/organizations" className="dropdown-link w-dropdown-link">
-                                    find an ngo
-                                </Link>
-                                <a
-                                    href="current-opportunities.html"
-                                    className="dropdown-link w-dropdown-link"
-                                >
-                                    CURRENT PROJECTS
-                                </a>
-                            </nav>
-                        </div>
-                        <Link to="/about" className="navbarlink w-nav-link">
-                            About
-                        </Link>
-                        <Link to="/contact-us" className="navbarlink w-nav-link">
-                            Contact
-                        </Link>
-                    </nav>
-                    <div className="w-nav-button">
-                        <div className="w-icon-nav-menu" />
-                    </div>
-                    <LoginHandler />
-                </div>
-            </div>
+            <MDBNavbar color="#444444" style={{ paddingRight: "6rem" }} dark expand="md">
+                <MDBNavbarBrand link="/">
+                    <img src="images/Fair-shotsV3.png" width="250" />
+                </MDBNavbarBrand>
+                <MDBNavbarToggler onClick={this.toggleCollapse} />
+                <MDBCollapse id="navbarCollapse3" isOpen={this.state.isOpen} navbar>
+                    <MDBNavbarNav left>
+                        <MDBNavItem>
+                            <MDBDropdown>
+                                <MDBDropdownToggle nav color="#444444" caret>
+                                    ORGANIZATIONS
+                                </MDBDropdownToggle>
+                                <MDBDropdownMenu right>
+                                    <MDBDropdownItem
+                                        onClick={this.redirect("/register#organization")}
+                                    >
+                                        REGISTER AN ORGANIZATION
+                                    </MDBDropdownItem>
+                                    {this.props.isAuthenticated && (
+                                        <MDBDropdownItem
+                                            onClick={this.redirect("/create-a-project")}
+                                        >
+                                            CREATE A NEW PROJECT
+                                        </MDBDropdownItem>
+                                    )}
+                                    <MDBDropdownItem onClick={this.redirect("/photographers")}>
+                                        FIND A PHOTOGRAPHER
+                                    </MDBDropdownItem>
+                                </MDBDropdownMenu>
+                            </MDBDropdown>
+                        </MDBNavItem>
+                        <MDBNavItem>
+                            <MDBDropdown>
+                                <MDBDropdownToggle nav color="#444444" caret>
+                                    PHOTOGRAPHERS
+                                </MDBDropdownToggle>
+                                <MDBDropdownMenu right>
+                                    <MDBDropdownItem
+                                        onClick={this.redirect("/register#photographer")}
+                                    >
+                                        REGISTER AS A PHOTOGRAPHER
+                                    </MDBDropdownItem>
+                                    <MDBDropdownItem onClick={this.redirect("/organizations")}>
+                                        FIND AN NGO
+                                    </MDBDropdownItem>
+                                    <MDBDropdownItem onClick={this.redirect("/projectss")}>
+                                        CURRENT PROJECTS
+                                    </MDBDropdownItem>
+                                </MDBDropdownMenu>
+                            </MDBDropdown>
+                        </MDBNavItem>
+                        <MDBNavItem>
+                            <MDBNavLink to="/about">ABOUT</MDBNavLink>
+                        </MDBNavItem>
+                        <MDBNavItem>
+                            <MDBNavLink to="/contact-us">CONTACT</MDBNavLink>
+                        </MDBNavItem>
+                    </MDBNavbarNav>
+                    <MDBNavbarNav right>
+                        <LoginHandler />
+                    </MDBNavbarNav>
+                </MDBCollapse>
+            </MDBNavbar>
         );
     }
 }
+
+export default NavbarPage;
