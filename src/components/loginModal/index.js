dc82533d7c647182718b36ca6e920028f541cbac
arunkumar413 <arunkumar413@gmail.com>
2019-06-21 12:48:17 +0530

    added the @babel/runtime-corejs2 in the package.json file and aligned the login modal elements

--- src/components/loginModal/index.js
+++ src/components/loginModal/index.js
@@ -8,7 +8,8 @@ import {
     Modal,
     ModalBody,
     ModalHeader,
-    Row
+    Row,
+    Col
 } from "reactstrap";
 
 import { FacebookLoginButton,GoogleLoginButton,InstagramLoginButton } from "react-social-login-buttons";
@@ -22,11 +23,11 @@ export default function LoginModal(props) {
             name="loginModal"
             isOpen={props.showModal}
             toggle={() => props.showLoginModal("loginModal")}>
-            <ModalHeader toggle={() => props.showLoginModal("loginModal")}> <h4> Please signin to access you account</h4> </ModalHeader>
+            <ModalHeader  toggle={() => props.showLoginModal("loginModal")}> <h4>  Please signin to access you account</h4> </ModalHeader>
             <ModalBody>
                 <Form onSubmit={props.handleSubmit}>
                     <Row className="justify-content-center">
-                        <FormGroup className="mb-2 mr-sm-2 mb-sm-2">
+                        <FormGroup className="mb-2 mr-sm-2 mb-sm-2" style={{width:'50%'}}>
                             <Input
                                 type="email"
                                 name="email"
@@ -36,9 +37,10 @@ export default function LoginModal(props) {
                                 onChange={props.handleChange}
                                 invalid={props.errorMessage !== ""}/>
                         </FormGroup>
+
                     </Row>
                     <Row className="justify-content-center">
-                        <FormGroup className="form-group mb-2 mr-sm-2 mb-sm-2">
+                        <FormGroup className="form-group mb-2 mr-sm-2 mb-sm-2" style={{width:'50%'}}>
                             {!props.forgotPass && (<Input
                                 type="password"
                                 name="password"
@@ -66,7 +68,7 @@ export default function LoginModal(props) {
                         )}
                     </Row>
                     <Row className="justify-content-center m-2">
-                        <Button type="submit" color="success">
+                        <Button className = 'col-6'type="submit" color ='success' style={{width:'50%'}} >
                             {props.forgotPass
                                 ? "Send me an e-mail"
                                 : "Login"}
