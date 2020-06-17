459d776cc436477e7979da73aa0d9d920012f155
Carlos Correa <carloscorrea91@hotmail.com>
2020-06-17 10:57:31 +0100

    More filters options added

--- src/components/filterBox/index.js
+++ src/components/filterBox/index.js
@@ -3,11 +3,67 @@ import { Col, Form, FormGroup, Label, Input } from "reactstrap";
 
 import "./filterbox.scss";
 
-const FilterBox = ({ options, select, condition, handleChange }) => (
+const FilterBox = ({
+    options,
+    type,
+    language,
+    cause,
+    select,
+    skillType,
+    currentCause,
+    currentLanguage,
+    condition,
+    handleChange
+}) => (
     <Form>
-        <FormGroup row className="filter-row row justify-content-end">
+        {/* justify-content-end changed to justify-content-center */}
+        <FormGroup row className="filter-row row justify-content-center">
+            {type ? (
+                <Col sm={2} className="d-inline-flex">
+                    <Input
+                        type="select"
+                        value={skillType}
+                        name="select"
+                        onChange={handleChange}
+                        id="selectType"
+                    >
+                        {type.map(o => (
+                            <option>{o}</option>
+                        ))}
+                    </Input>
+                </Col>
+            ) : (
+                <Col style={{ display: "none" }} />
+            )}
+            {/* Active when database is available for the following: */}
             <Col sm={2} className="d-inline-flex">
-                <Label for="exampleSelect">Filter by:</Label>
+                <Input
+                    type="select"
+                    value={currentCause}
+                    name="select"
+                    onChange={handleChange}
+                    id="selectCause"
+                >
+                    {cause.map(o => (
+                        <option>{o}</option>
+                    ))}
+                </Input>
+            </Col>
+            <Col sm={2} className="d-inline-flex">
+                <Input
+                    type="select"
+                    value={currentLanguage}
+                    name="select"
+                    onChange={handleChange}
+                    id="selectLanguage"
+                >
+                    {language.map(o => (
+                        <option>{o}</option>
+                    ))}
+                </Input>
+            </Col>
+            <Col sm={2} className="d-inline-flex">
+                {/* <Label for="exampleSelect">Filter by:</Label> */}
 
                 <Input
                     type="select"
